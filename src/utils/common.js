92734d86838870c4adca41cd6c21a03538c6e3fb
Serkik <63666583+Serckik@users.noreply.github.com>
2023-06-13 03:51:30 +0500

    Большие перемены (часть 1)

--- src/utils/common.js
+++ src/utils/common.js
@@ -7,8 +7,22 @@ const getRandomInteger = (a = 0, b = 1) => {
 
 const getRandomElement = (elements) => {
   const MIN = 0;
-  const max = elements.length - 1;
-  return elements[getRandomInteger(MIN, max)];
+  const MAX = elements.length - 1;
+  return elements[getRandomInteger(MIN, MAX)];
 };
 
-export { getRandomInteger, getRandomElement };
+const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1),
+  ];
+};
+
+export { getRandomInteger, updateItem, getRandomElement };
